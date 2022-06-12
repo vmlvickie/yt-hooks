d46dcda4006c552339b276ad3d8235adb17c873c
victor lijoodi <victor.lijoodi@gmail.com>
2022-06-12 17:41:16 +0300

    remove unused import

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import SearchBar from "./SearchBar";
 import VideoList from "./VideoList";
 import VideoDetail from "./VideoDetail";
